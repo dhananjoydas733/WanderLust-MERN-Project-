91479c086b5606d46376a6df38473c6dfc7c707a
Dhananjoy Das <162976473+dhananjoydas733@users.noreply.github.com>
2024-12-22 19:21:46 +0530

    Errorfixed

--- models/listing.js
+++ models/listing.js
@@ -8,15 +8,13 @@ const listingSchema = new Schema({
     },
     description: String,
     image: {
-        // type: String,
-        filename: {
-            type: String,
-        },
-        url: {
-            type: String,
-            default: "https://images.pexels.com/photos/4428289/pexels-photo-4428289.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-             set: (v) => v === "" ? "https://tse1.mm.bing.net/th?id=OIP.n1d8R3WXkeDwYZjKL0qnOQHaFj&pid=Api&P=0&h=180" : v,
-        },
+        type: String,
+        default: 
+            "https://images.pexels.com/photos/4428289/pexels-photo-4428289.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        set: (v) =>
+            v === "" 
+                ? "https://images.pexels.com/photos/4428289/pexels-photo-4428289.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" 
+                    : v,
     },
     price: Number,
     location: String,
